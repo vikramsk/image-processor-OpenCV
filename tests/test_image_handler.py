f3046ac23603b1b7d529f760b3a7e113e44b948b
Vikram Sreekumar <vikram1791@gmail.com>
2015-04-05 16:01:31 +0530

    Removed unused dependencies

--- tests/test_image_handler.py
+++ tests/test_image_handler.py
@@ -6,7 +6,7 @@ import scripts.image_handler as image_handler
 
 class ImageHandlerTests(TestCase):
     def test_get_sift_descriptor(self):
-        image_path = ""
+        image_path = "D:\CodeBase\Python CodeBase\Data\\3_Star_Hotels_Images\\313404\EntireSet\\19413278.jpg"
         image_file = cv2.imread(image_path, cv2.CV_LOAD_IMAGE_GRAYSCALE)
         i_handler = image_handler.ImageHandler(image_file)
         sift_descriptors = i_handler.get_sift_descriptor()
--- tests/test_image_operations.py
+++ tests/test_image_operations.py
@@ -9,15 +9,15 @@ import scripts.image_handler as image_handler
 
 class ImageHandlerTests(TestCase):
     def test_load_image_descriptors_from_folder(self):
-        folder_path = ""
+        folder_path = "D:\CodeBase\Python CodeBase\Data\\3_Star_Hotels_Images\\313404\EntireSet\\"
         image_descriptors = image_operations.ImageOperations().load_image_descriptors_from_folder(folder_path)
         self.assertGreater(list(image_descriptors).__sizeof__(), 0)
 
     def test_get_similar_images(self):
-        folder_path = ""
+        folder_path = "D:\CodeBase\Python CodeBase\Data\\3_Star_Hotels_Images\\313404\EntireSet\\"
         image_operator = image_operations.ImageOperations()
         image_operator.load_image_descriptors_from_folder(folder_path)
-        image_path = ""
+        image_path = "D:\CodeBase\Python CodeBase\Data\\3_Star_Hotels_Images\\313404\EntireSet\\8850442.jpg"
 
         image_file = cv2.imread(image_path, cv2.CV_LOAD_IMAGE_GRAYSCALE)
         image_descriptor = image_handler.ImageHandler(image_file).get_sift_descriptor()
@@ -26,8 +26,8 @@ class ImageHandlerTests(TestCase):
         self.assertGreater(list(similar_images).__sizeof__(), 0)
 
     def test_get_unique_images(self):
-        folder_path = ""
-        dest_folder = ""
+        folder_path = "D:\CodeBase\Python CodeBase\Data\\3_Star_Hotels_Images\\313404\EntireSet\\"
+        dest_folder = "G:\\Trash\\"
         image_operator = image_operations.ImageOperations()
 
         file_list = {}
